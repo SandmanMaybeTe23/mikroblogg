71b2aa9c0bb7b69f5071d73b1d8b59b1e3177e1c
billy <billy.burlin@elev.ga.ntig.se>
2026-02-13 11:09:10 +0100

    Security assignment

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,37 @@ import pool from "../config/database.js"
 
 const router = express.Router()
 
-router.get("/", async (req, res, next) => {
-    try {
-        const [rows] = await pool.query("SELECT * FROM post ORDER BY created_at DESC")
-        res.json(rows)
-    } catch (err) {
-        next(err)
-    }
-})
+import { param, validationResult } from "express-validator"
+
+router.get(
+    "/posts/:id",
+    param("id").isInt().withMessage("Inläggs-ID måste vara ett heltal."),
+    async (req, res, next) => {
+        try {
+            const errors = validationResult(req)
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() })
+            }
+
+            const postId = req.params.id
+
+            const [rows] = await pool.query(
+                `SELECT post.id, post.content, post.created_at, user.name
+                FROM post
+                JOIN user ON post.user_id = user.id
+                WHERE post.id = ?`,
+                [postId],
+            )
+
+            if (rows.length === 0) {
+                throw new Error("Inlägget kunde inte hittas.")
+            }
+
+            res.json(rows[0])
+        } catch (err) {
+            next(err)
+        }
+    },
+)
 
 export default router
